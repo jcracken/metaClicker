172e72c1938bd69b43a236c3143a940d25c83389
Tam <Tam@10.134.213.160>
2016-11-22 12:14:00 -0700

    Added lifetime counter just for statistics tab

--- metaclicker/src/metaClickerPrototype/Clicker.java
+++ metaclicker/src/metaClickerPrototype/Clicker.java
@@ -7,17 +7,24 @@ import java.io.*;
 public class Clicker extends JFrame {
 
 	private double counter;
+	private double lifetimeCounter;
 
 	public Clicker() {
 		counter = 0;
+		lifetimeCounter = 0;
 	}
 
 	public void click() {
 		counter++;
+		lifetimeCounter++;
 	}
 
 	public double getCounter() {
 		return counter;
+	}
+	
+	public double getLifetimeCounter() {
+		return this.lifetimeCounter;
 	}
 
 //	public static void main(String[] args) {
