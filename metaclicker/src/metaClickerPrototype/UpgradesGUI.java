8c50108e8b33995490c5242f3477bcf5d2cf328c
jimin <jimin@10.134.229.184>
2016-11-22 11:29:28 -0700

    Fleshed out num of upgrades

--- metaclicker/src/metaClickerPrototype/UpgradesGUI.java
+++ metaclicker/src/metaClickerPrototype/UpgradesGUI.java
@@ -6,6 +6,18 @@ import java.awt.event.*;
 
 public class UpgradesGUI extends JFrame{
 	
+	private ActiveUpgrades active1;
+	private ActiveUpgrades active2;
+	private ActiveUpgrades active3;
+	private ActiveUpgrades active4;
+	private ActiveUpgrades active5;
+	
+	private PassiveUpgrades passive1;
+	private PassiveUpgrades passive2;
+	private PassiveUpgrades passive3;
+	private PassiveUpgrades passive4;
+	private PassiveUpgrades passive5;
+	
 	public UpgradesGUI() {
 		super("Upgrades");
 		
@@ -14,6 +26,19 @@ public class UpgradesGUI extends JFrame{
 		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		
 		setVisible(true);
+		
+		createUpgrades();
+		
+		createUpgradesPanel();
+		
+	}
+	
+	public void createUpgrades() {
+		//in which we create all of the upgrades and initialize their values
+	}
+	
+	public void createUpgradesPanel() {
+		//in which we draw the ui for purchasing upgrades
 	}
 
 }
