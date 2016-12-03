74459b119555a31fa926f6c15f18608427435793
jimin <jimin@RohinSP4>
2016-12-03 11:13:46 -0700

    dem upgrades

--- metaclicker/src/metaClickerPrototype/ClickingWindow.java
+++ metaclicker/src/metaClickerPrototype/ClickingWindow.java
@@ -42,6 +42,7 @@ public class ClickingWindow extends JFrame {
 	private PassiveUpgrades passive3;
 	private PassiveUpgrades passive4;
 	private PassiveUpgrades passive5;
+	private PassiveUpgrades passive6;
 	
 	private JPanel firstUpgrade;
 	private JLabel firstUpgradeTitle;
@@ -209,64 +210,70 @@ public class ClickingWindow extends JFrame {
 	
 	private void initializeUpgrades() {
 		active1 = new ActiveUpgrades();
-		active1.setName("Click Multiplier");
-		active1.setDesc("You modified the button to increment by 2.");
+		active1.setName("Mouse Upgrade"); //first
+		active1.setDesc("You bought a new mouse that clicks twice every time you click (Upgrades click rate to 2).");
 		active1.setCost(10);
 		active1.setMult(2);
 		
 		active2 = new ActiveUpgrades();
-		active2.setName("Click Multiplier 2");
-		active2.setDesc("You modified the button to increment by 4.");
-		active2.setCost(100);
-		active2.setMult(4);
+		active2.setName("Run Cheat Engine"); //third
+		active2.setDesc("Use Cheat Engine to speed up the game (Upgrades click rate to 5).");
+		active2.setCost(50);
+		active2.setMult(5);
 		
 		active3 = new ActiveUpgrades();
-		active3.setName("Click Multiplier 3");
-		active3.setDesc("You modified the button to increment by 8.");
-		active3.setCost(1000);
-		active3.setMult(8);
+		active3.setName("Faster Processor"); //fifth
+		active3.setDesc("You bought a new CPU, congrats (Upgrades click rate to 20).");
+		active3.setCost(250);
+		active3.setMult(20);
 		
 		active4 = new ActiveUpgrades();
-		active4.setName("Click Multiplier 4");
-		active4.setDesc("You modified the button to increment by 16.");
-		active4.setCost(10000);
-		active4.setMult(16);
+		active4.setName("Trumpacolypse"); //seventh
+		active4.setDesc("Trump gets elected president and the world is about to end, but at least you won a bet (Upgrades click rate to 50).");
+		active4.setCost(750);
+		active4.setMult(50);
 		
 		active5 = new ActiveUpgrades();
-		active5.setName("Click Multiplier 5");
-		active5.setDesc("You modified the button to increment by 32.");
-		active5.setCost(100000);
-		active5.setMult(32);
+		active5.setName("Utopia"); //nine
+		active5.setDesc("You find a Xenomorph alien civilization with no war and everlasting peace (Upgrades click rate to 100).");
+		active5.setCost(10000);
+		active5.setMult(100);
 		
 		passive1 = new PassiveUpgrades();
-		passive1.setCost(15);
-		passive1.setDesc("You used your knowledge in computer programming to set up a shoddy auto clicker.");
-		passive1.setName("Auto-Clicker");
+		passive1.setCost(20);
+		passive1.setDesc("You used your knowledge in computer programming to set up a shoddy auto clicker (Increases passive clicks by one).");
+		passive1.setName("Auto-Clicker"); //second
 		passive1.setCPS(1);
 		
 		passive2 = new PassiveUpgrades();
-		passive2.setCost(25);
-		passive2.setDesc("You properly understand coding syntax and create a second program to optimize");
-		passive2.setName("Optimizer");
-		passive2.setCPS(2);
+		passive2.setCost(100);
+		passive2.setDesc("You properly understand coding syntax and create a second program to optimize. (Increases passive clicks by five).");
+		passive2.setName("Execute Optimizer"); //fourth
+		passive2.setCPS(5);
 		
 		passive3 = new PassiveUpgrades();
-		passive3.setCost(35);
-		passive3.setDesc("You decide this game sucks and cheat by modifying game files to run multiple instances");
-		passive3.setName("Clone");
-		passive3.setCPS(3);
+		passive3.setCost(500);
+		passive3.setDesc("You install an ActionKey script that clicks in the background for you (Increases passive clicks by ten).");
+		passive3.setName("ActionKey Script"); //sixth
+		passive3.setCPS(10);
 		
 		passive4 = new PassiveUpgrades();
-		passive4.setCost(45);
-		passive4.setDesc("You cheat by hiring a bunch of monkeys to click for you");
-		passive4.setName("Monkeys");
-		passive4.setCPS(4);
+		passive4.setCost(10000); //eight
+		passive4.setDesc("You go to space (Increases passive clicks by 20).");
+		passive4.setName("Space Exploration");
+		passive4.setCPS(20);
 		
 		passive5 = new PassiveUpgrades();
-		passive5.setCost(55);
-		passive5.setDesc("Harambe, our lord and savior, felt insulted that you went after his kin, therefore he took over the whole operation.");
-		passive5.setName("Good ol' Harambe");
-		passive5.setCPS(5);
+		passive5.setCost(1000000);
+		passive5.setDesc("Harambe for the assist! Gives you an edge on those alien bastards who shot up your ride (Increases passive clicks to -150).");
+		passive5.setName("clicks out for Harambe"); //eleven
+		passive5.setCPS(86);
+		
+		passive6 = new PassiveUpgrades();
+		passive6.setCost(500000);
+		passive6.setDesc("KILL KILL KILL (Decreases passive clicks to -200).");
+		passive6.setName("Xenomorph War"); //ten
+		passive6.setCPS(-236); //if this upgrade is bought and hits zero, you lose. otherwise, you get a win dialog after 20 min.
 	}
 	
 	public void createUpgradesPanel() {
